f924676b7f065c4f12be2138fc94049953f7a3ba
Russell Endicott <rendicott@gmail.com>
2018-02-25 11:06:01 -0500

    Adding the startup script that fixes synatpics sensitivity stuff

---
+++ fixsyntouch.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Sets the synaptics touchpad settings to be more sane
+# on an HP Spectre 2016 V8T99UA
+xinput --set-prop 15 275 3
+xinput --set-prop 15 313 .6 1.2 .1 0.00
+xinput --set-prop 15 274 1.5
